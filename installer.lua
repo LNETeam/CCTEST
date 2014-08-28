9e53597319042f917038fb7c4fee9cee0d8e2a30
LNETeam <lneteam@hotmail.com>
2014-08-28 00:14:21 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -2,12 +2,12 @@ term.clear()
 term.setCursorPos(1,1)
 install = false
 function drawOne()
-	term.setBackgroundColor(colors.grey)
+	term.setBackgroundColor(colors.gray)
 	term.clear()
 	term.setBackgroundColor(colors.white)
 	term.setCursorPos(1,1)
 	term.clearLine()
-	term.setBackgroundColor(colors.grey)
+	term.setBackgroundColor(colors.gray)
 	term.setTextColor(colors.black)
 	term.setCursorPos(1,3)
 	print("Install startup executable? [y/n]")
@@ -17,13 +17,13 @@ while true do
 	drawOne()
 	local t = os.startTimer(1)
 	local e = {os.pullEvent()}
-	if (e[1]=="key" and e[2] == 21)
+	if (e[1]=="key" and e[2] == 21) then
 		install = true
 		break
-	elseif (e[1]=="key" and e[2] == 49)
+	elseif (e[1]=="key" and e[2] == 49) then
 		install = false
 		break
-	elseif (e[1]=="timer")
+	elseif (e[1]=="timer") then
 
 	end
 end
